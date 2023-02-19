92de00c9c405bdcf256945aaad0c715b5422308b
mkmkmkh <122198010+mkmkmkh@users.noreply.github.com>
2023-02-20 00:35:39 +0900

    Add files via upload

--- js/comment.js
+++ js/comment.js
@@ -1,17 +1,18 @@
 function submitComment() {
   // Get user inputs
-  let name = document.getElementById("name").value;
-  let comment = document.getElementById("comment").value;
-  
+  let my_id = document.getElementById("my_id").value;
+  let friend_id = document.getElementById("friend_id").value;
+  let sns_id = document.getElementById("sns_name").value;
   // Create new comment element
   let newComment = document.createElement("li");
-  newComment.innerHTML = "<strong>" + name + ":</strong> " + comment;
+  newComment.innerHTML = "<h3><strong>" + sns_name + ":</strong> " + '내ID: ' +my_id+',친구ID: ' + friend_id + '</h3>';
   
   // Append new comment to comment list
   let commentList = document.getElementById("comment-list");
   commentList.appendChild(newComment);
   
   // Clear form inputs
-  document.getElementById("name").value = "";
-  document.getElementById("comment").value = "";
+  document.getElementById("my_id").value = "";
+  document.getElementById("friend_id").value = "";
+  document.getElementById("sns_name").value = "";
 }
